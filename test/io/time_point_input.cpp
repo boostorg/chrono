3d8ea1d1e1aa60f091b4f8cac5ee504b29e8b023
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-05-01 03:34:52 +0200

    avoid warnings.

--- test/io/time_point_input.cpp
+++ test/io/time_point_input.cpp
@@ -158,7 +158,7 @@ void check_all_process_cpu_clock()
 {
   using namespace boost::chrono;
   using namespace boost;
-  typedef process_cpu_clock Clock;
+  //typedef process_cpu_clock Clock;
   //test_good<Clock> ("{5000;0;0} nanoseconds", process_cpu_clock::duration(process_cpu_clock::times(5000,0,0)));
 }
 #endif
